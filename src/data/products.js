6ad605bc4e4e0fcdb0fcd5e5a7e13a01e2e2bf0c
fiqih <andrebian240907@gmail.com>
2025-07-23 02:56:52 +0700

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -56,14 +56,19 @@ export const products = [
       '/topi/topi3.webp'
     ],
     description: [
-      'Trendy hoodie with comfortable fit',
-      'Premium quality material with comfortable cotton blend',
+      'Look trendy and casual with the Brown Stellaire Luminaire Trucker Hat! This hat is designed for maximum comfort and timeless style. Crafted from quality materials, the front of the hat is made of sturdy fabric in an elegant brown color, while the back uses mesh to ensure good air circulation, keeping your head cool during your activities.',
+      '\n',
+      'On the front, this hat is adorned with a premium embroidered shield-and-crown logo, adding an exclusive and classy touch. The word "STELLAIRE" embroidered below the logo adds a luxurious touch. Another unique detail is the words "LASH THE WORLD WITH YOUR SMILE" embroidered on the right side, conveying a positive and stylish message.',
+      '\n',
       'Available in multiple colors: black, navy, gray',
       '\n',
       'Features:',
-      '- Adjustable drawstrings',
-      '- Kangaroo pocket',
-      '- Ribbed cuffs and hem'
+      '- High-Quality Materials: A combination of solid fabric on the front and mesh on the back for comfort and durability.',
+      '- Unique & Stylish Design: The shield-and-crown logo and neat embroidered text.',
+      '- Optimal Air Circulation: The mesh section prevents sweating.',
+      '- Universal Fit: Featuring a snapback adjustment, it can be adjusted to fit various head sizes.',
+      '\n',
+      "Perfect for everyday wear, whether you're hanging out, working out, or just hanging out. Get the Brown Stellaire Luminaire Trucker Hat now and add a cool touch to your look!"
     ],
     link: 'https://wa.me/6285922065696'
   },
